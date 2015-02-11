f59831e1a0562bc01b9f850697c11a8c944b6178
Christian Hoogerheyde <choogerheyde@gmail.com>
2015-02-11 15:18:14 -0500

    Problem 2 flesh out
    replicated Fibonacci sequence, setup for eventual summing

--- problem_2.py
+++ problem_2.py
@@ -5,5 +5,32 @@ Created on Mon Dec 08 21:29:50 2014
 @author: choog_000
 """
 
-list = [1,2]
-list.append()
+startList = [1,2]
+values = [1]
+
+#reproduces the Fibonacci sequence up to a value which does not exceed the one
+#set in the first line below
+
+if startList[-1] < 500:    
+    for item in values:
+        startList.append((startList[-2] + startList[-1]))
+else:
+    print "The sequence's value has exceeded four million."
+  
+#initiates a list of even-valued terms  
+evenTerms = [0]
+
+#iterates over all values in the Fibonacci sequence to determine which values
+#to include in the evenTerms list
+for item in startList:
+    if item%2 == 0:
+        evenTerms.append(item)
+    else:
+ 
+#Sums all even-valued terms from our new list, providing us with our answer       
+daSum = 
+
+startList[0] + startList[1]
+print newList
+
+startList[0] + startList[1]
