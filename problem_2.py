7c731c34bce0fda56ae1963f21e797582dd442e7
Christian Hoogerheyde <choogerheyde@gmail.com>
2015-02-17 18:09:37 -0500

    Completing problem 2 about fibonacci sequence

--- problem_2.py
+++ problem_2.py
@@ -4,33 +4,48 @@ Created on Mon Dec 08 21:29:50 2014
 
 @author: choog_000
 """
+##Project Euler: Problem 2 - Even Fibonacci Numbers
 
-startList = [1,2]
-values = [1]
+fibList = [1,2]
+values = range(35)
+fourMil = 4000000
 
-#reproduces the Fibonacci sequence up to a value which does not exceed the one
-#set in the first line below
+#reproduces the Fibonacci sequence up to a value which does not exceed 
+#four million
 
-if startList[-1] < 500:    
-    for item in values:
-        startList.append((startList[-2] + startList[-1]))
+for item in values:
+    if fibList[-1] < fourMil:    
+        fibList.append((fibList[-2] + fibList[-1]))
+    else:
+        print "The sequence's value has now exceeded four million."
+
+#to test the sequence's length and to set an appropriate "values" element
+print len(fibList)
+#turns out fibList is around 33
+
+#the last term, which summed the final two terms in the fibList, should now 
+#exceed fourMil. Below chops off the last number to ensure we stay below the limit.
+
+if fibList[-1] > fourMil:
+    del fibList[-1]    
 else:
-    print "The sequence's value has exceeded four million."
-  
+    print "The final value in the sequence did not exceed four million."
+    
+print fibList
+
 #initiates a list of even-valued terms  
 evenTerms = [0]
 
 #iterates over all values in the Fibonacci sequence to determine which values
 #to include in the evenTerms list
-for item in startList:
+for item in fibList:
     if item%2 == 0:
         evenTerms.append(item)
     else:
- 
-#Sums all even-valued terms from our new list, providing us with our answer       
-daSum = 
+        print "Odd number"
 
-startList[0] + startList[1]
-print newList
-
-startList[0] + startList[1]
+print evenTerms
+        
+#Sums the elements from our new evenTerms list, providing us with our answer       
+daSum = sum(evenTerms)
+print "Your final answer is " + str(daSum)
