630f99717921ef1a735a93f079977aff54ed4456
Christian Hoogerheyde <choogerheyde@gmail.com>
2015-09-26 23:31:57 -0400

    using square root trick to limit the number of items in list of factors --- now solved!

--- problem_3.py
+++ problem_3.py
@@ -1,39 +1,29 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Apr 12 21:20:27 2015
-
-@author: choog_000
-"""
+import math
 ##Project Euler -- Problem 3
+#What is the largest prime factor of 600851475143?
+
+# test function to execute --> PrimeFactorMetaFxn(13195)
+# function to execute --> PrimeFactorMetaFxn(600851475143)
+
+#meta function to bring it all together
 def PrimeFactorMetaFxn(daNum):
     answer = max_prime_factor(is_prime(list_all_factors(daNum)))
-    #is_prime(factorList)
-    #return max_prime_factor(primeFactors)
     print "The answer is " + str(answer)    
     return answer         
-##the below functions work and correctly
-##generate the test answer (29 for test 13195)
-##however, receiving an overflow error 'range()
-##result has too many items'
   
-#def daPrimeFactorMetaFxn(daNum, factorList, primeFactors):
-#    list_all_factors(daNum)
-#    is_prime(factorList)
-#    answer = max_prime_factor(primeFactors)
-#    print "The answer is " + str(answer)
-#  
+#create a list of all possible factors
 def list_all_factors(daNum):
     factorList = []
-    possFactor = xrange(1,daNum)
-    
+    daNumRoot = math.sqrt(daNum)
+    possFactor = xrange(1,int(daNumRoot))
     #loop through range up to daNum, id those which are factors
     for x in possFactor:
         if daNum%x == 0:
             factorList.append(x)
     
-    #print "factorList is " + str(factorList)
     return factorList
 
+#determine which items in list are prime
 def is_prime(factorList):
     primeFactors = []    
     for factor in factorList:
@@ -48,7 +38,8 @@ def is_prime(factorList):
             primeFactors.append(factor)    
     print "primeFactors are " + str(primeFactors)
     return primeFactors 
-    
+   
+#return the highest prime factor
 def max_prime_factor(primeFactors):
     maximumPrime = max(primeFactors)
     #print "maximumPrime is " + str(maximumPrime)
